6bd6cdf0463532f953674b1cb0bc0ae1ae919e35
Eric Song <ericksong91@gmail.com>
2023-01-17 19:49:33 -0500

    done with assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie(dev, item_name, value)
+        new = Freebie.create(dev_id: Dev.find_by(name: dev).id, company_id: self.id, value: value, item_name: item_name)
+        new.save
+    end
+
+    def self.oldest_company
+        year = Company.all.minimum(:founding_year)
+        Company.find_by(founding_year: year)
+    end
 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        if self.freebies.where(item_name: item_name).count > 0
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+            freebie.save
+        else
+            "You don't own this!"
+        end
+
+    end
 
 end
